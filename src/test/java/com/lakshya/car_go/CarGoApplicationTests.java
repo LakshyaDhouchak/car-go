7d0c47f241315406916750165dbcc25f5caca3e9
lakshya <lakshya10171@gmail.com>
2025-10-02 10:52:30 +0530

    base commit

---
+++ src/test/java/com/lakshya/car_go/CarGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakshya.car_go;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
