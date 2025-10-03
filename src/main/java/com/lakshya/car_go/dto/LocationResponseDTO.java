3b988108fb1b84c1110915419d7605d47e3c390d
lakshya <lakshya10171@gmail.com>
2025-10-03 22:04:21 +0530

    add location Response DTO

---
+++ src/main/java/com/lakshya/car_go/dto/LocationResponseDTO.java
@@ -0,0 +1,14 @@
+package com.lakshya.car_go.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class LocationResponseDTO {
+    // define the properties
+
+    private Long id;
+    private String name;
+    private String Address;
+}
