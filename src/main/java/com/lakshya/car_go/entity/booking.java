3d58cd0e87b7246a21b576c1b9c283cae1651ab9
lakshya <lakshya10171@gmail.com>
2025-10-03 06:22:13 +0530

    implemetiation of booking entity

---
+++ src/main/java/com/lakshya/car_go/entity/booking.java
@@ -0,0 +1,47 @@
+package com.lakshya.car_go.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "booking")
+@Data
+@NoArgsConstructor
+public class booking {
+    // define the properties
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",nullable=false)
+    private user user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "car_id",nullable = false)
+    private car car;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDate starDate;
+
+    @Column(name = "end_date",nullable = false)
+    private LocalDate endDate;
+
+    @Column(name="booking_status",nullable = false,length=20 )
+    private String bookingStatus;
+
+    @Column(name ="total_price",nullable = false,precision = 10,scale = 2)
+    private Double totalPrice;
+}
