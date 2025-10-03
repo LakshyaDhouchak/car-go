5da11e3ae676f0535008f561ae512c1f1e46f293
lakshya <lakshya10171@gmail.com>
2025-10-03 19:36:36 +0530

    implemented created table

---
+++ src/main/resources/db/migration/V6__create_table.sql
@@ -0,0 +1,41 @@
+
+-- User Table:
+CREATE TABLE IF NOT EXISTS user(
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    hashed_password VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Location Table:
+CREATE TABLE IF NOT EXISTS location(
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    address VARCHAR(100) NOT NULL
+);
+
+-- Car Table:
+CREATE Table IF NOT EXISTS car(
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    license_plate VARCHAR(20) UNIQUE NOT NULL,
+    daily_rate DECIMAL(10,2) NOT NULL,
+    car_status VARCHAR(12) NOT NULL,
+    current_location_id BIGINT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(current_location_id) REFERENCES location(id)
+);
+
+-- Booking Table:
+CREATE TABLE IF NOT EXISTS booking(
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    car_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    start_date Date NOT NULL,
+    end_date Date NOT NULL,
+    bookingStatus VARCHAR(20) NOT NULL,
+    totalPrice DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY(car_id) REFERENCES car(id),
+    FOREIGN KEY(user_id) REFERENCES user(id)
+);
