e9293be678193d9f417f8194259df5b09af641be
lakshya <lakshya10171@gmail.com>
2025-10-03 07:28:43 +0530

    implementation of car repo

---
+++ src/main/java/com/lakshya/car_go/repository/userRepository.java
@@ -0,0 +1,9 @@
+package com.lakshya.car_go.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.lakshya.car_go.entity.user;
+
+public interface userRepository extends JpaRepository<user ,Long> {
+
+    
+} 
