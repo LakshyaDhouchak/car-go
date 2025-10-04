322f63067f29b963b59c21c744b757f4031bc9f9
lakshya <lakshya10171@gmail.com>
2025-10-05 00:14:40 +0530

    updated userRepository

--- src/main/java/com/lakshya/car_go/repository/userRepository.java
+++ src/main/java/com/lakshya/car_go/repository/userRepository.java
@@ -1,9 +1,13 @@
 package com.lakshya.car_go.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.lakshya.car_go.entity.user;
 
 public interface userRepository extends JpaRepository<user ,Long> {
+
+    Optional<user> findByEmail(String email);
 
     
 } 
