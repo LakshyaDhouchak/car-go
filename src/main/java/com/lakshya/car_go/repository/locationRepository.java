fc3016229e4ee4eb2555995ab5bc3c6c9e5e2a47
lakshya <lakshya10171@gmail.com>
2025-10-05 00:15:09 +0530

    updated locationRepository

--- src/main/java/com/lakshya/car_go/repository/locationRepository.java
+++ src/main/java/com/lakshya/car_go/repository/locationRepository.java
@@ -1,8 +1,10 @@
 package com.lakshya.car_go.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.lakshya.car_go.entity.location;
 
 public interface locationRepository  extends JpaRepository<location , Long>{
-    
-}
+    Optional<location> findByNameAndAddress(String name, String Address);
+}   
