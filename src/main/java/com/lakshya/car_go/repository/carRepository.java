4aa86bc309537f9bea43a847d9ba4e991d3a5ea3
lakshya <lakshya10171@gmail.com>
2025-10-05 19:30:49 +0530

    updated car Repo

--- src/main/java/com/lakshya/car_go/repository/carRepository.java
+++ src/main/java/com/lakshya/car_go/repository/carRepository.java
@@ -1,8 +1,15 @@
 package com.lakshya.car_go.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.lakshya.car_go.entity.car;
 
 public interface carRepository  extends JpaRepository<car,Long>{
-    
+    // define the properties
+    Optional<car> findByLicensePlate(String licensePlate);
+
+    boolean existsByLicensePlate(String licensePlate);
+
+    void deleteByLicensePlate(String licensePlate);
 }
