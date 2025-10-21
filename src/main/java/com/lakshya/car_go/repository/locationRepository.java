a3003aa53427c537d4a83f4200ed314ff84d4a73
lakshya <lakshya10171@gmail.com>
2025-10-21 10:02:48 +0530

    add location repo

--- src/main/java/com/lakshya/car_go/repository/locationRepository.java
+++ src/main/java/com/lakshya/car_go/repository/locationRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.lakshya.car_go.entity.location;
 
 public interface locationRepository  extends JpaRepository<location , Long>{
-    Optional<location> findByNameAndAddress(String name, String Address);
+    Optional<location> findByNameAndAddress(String name, String address);
 }   
